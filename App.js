c524b576ea640a5074ce2de5122711d2192528bb
ercantekeli <ercantekeli@outlook.com>
2022-02-14 23:53:13 +0300

    last commit

--- App.js
+++ App.js
@@ -9,11 +9,30 @@ let toButton = document.getElementById('todobutton');
 
 let toDo = document.getElementById('lists');
 
+let addList = document.getElementById("task__list");
 
 
-toButton.addEventListener('click', () => {
-    let addList = `<input type="checkbox" name="ch" id="ch">
-    <label style="color: white;" for="ch">${toText.value}</label> <br>`;
 
-    toDo.innerHTML += addList;
+toButton.addEventListener("click", () => {
+    const li = document.createElement('li');
+    const input = document.createElement("input");
+    const label = document.createElement("label");
+    const del_icon = document.createElement("a");
+    del_icon.appendChild(document.createTextNode("❌"));
+    li.className = 'list-group-item';
+    del_icon.className = "del__icon"
+    input.className = "input_item";
+    input.setAttribute("type", "checkbox");
+    input.id = "süheyl";
+    label.setAttribute("for", "süheyl");
+    label.setAttribute("id", "mehmet");
+    label.appendChild(document.createTextNode(toText.value));
+    li.appendChild(input);
+    li.appendChild(label)
+    addList.appendChild(li);
+    toText.value = "";
+    li.appendChild(del_icon);
+    del_icon.addEventListener("click", (e) => {
+        e.target.parentElement.remove();
+    })
 });
